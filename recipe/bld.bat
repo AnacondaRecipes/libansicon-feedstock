10cab71ac923823dc426cd74b079238c4e858cbd
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-12-05 23:17:32 +0100

    try to build 64bit version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,14 @@
 echo on
 
-nmake BITS=32 /F makefile.vc
+nmake BITS=64 /F makefile.vc
 if errorlevel 1 exit 1
 
 
 copy %SRC_DIR%\ansicon.h %LIBRARY_INC%
 copy %SRC_DIR%\ansicon.rc %LIBRARY_INC%
-move %SRC_DIR%\x86\ANSI32.dll %LIBRARY_BIN%
-move %SRC_DIR%\x86\ANSI32.exp %LIBRARY_LIB%
-move %SRC_DIR%\x86\ANSI32.lib %LIBRARY_LIB%
+move %SRC_DIR%\x64\ansicon.exe %LIBRARY_BIN%
+move %SRC_DIR%\x64\ANSI64.dll %LIBRARY_BIN%
+move %SRC_DIR%\x64\ANSI64.exp %LIBRARY_LIB%
+move %SRC_DIR%\x64\ANSI64.lib %LIBRARY_LIB%
 
 
