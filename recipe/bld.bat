8304463faee34f76e5efe55abae278855765f6cc
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-12-05 22:52:49 +0100

    build 32bit version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,13 @@
-nmake BITS=64 /F makefile.vc
+echo on
+
+nmake BITS=32 /F makefile.vc
 if errorlevel 1 exit 1
+
+
+copy %SRC_DIR%\ansicon.h %LIBRARY_INC%
+copy %SRC_DIR%\ansicon.rc %LIBRARY_INC%
+move %SRC_DIR%\x86\ANSI32.dll %LIBRARY_BIN%
+move %SRC_DIR%\x86\ANSI32.exp %LIBRARY_LIB%
+move %SRC_DIR%\x86\ANSI32.lib %LIBRARY_LIB%
+
+
